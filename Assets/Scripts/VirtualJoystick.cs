349fd37d4b45b5eb8d198058b24400ffc86ae3ad
LEEJESEOK <junelle_js@naver.com>
2021-09-24 07:34:06 +0900

    Virtual Joystick 기본 동작 구현
    - TODO : 조이스틱 드래그에 따라 이동 처리

--- Assets/Scripts/VirtualJoystick.cs
+++ Assets/Scripts/VirtualJoystick.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class VirtualJoystick : MonoBehaviour
+public class VirtualJoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    [SerializeField]
+    RectTransform lever;
+    RectTransform rectTransform;
+
+    [SerializeField, Range(10f, 150f)]
+    private float leverRange;
+
+    Vector2 inputVector;
+    bool isInput;
+
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print(inputVector);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);
+        isInput = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        lever.anchoredPosition = Vector2.zero;
+        isInput = false;
+    }
+
+    public void ControlJoystickLever(PointerEventData eventData)
+    {
+        Vector2 inputDir = eventData.position - rectTransform.anchoredPosition;
+        Vector2 clampedDir = (inputDir.magnitude < leverRange) ?
+            inputDir : inputDir.normalized * leverRange;
+        lever.anchoredPosition = clampedDir;
+
+        inputVector = clampedDir / leverRange;
     }
 }
