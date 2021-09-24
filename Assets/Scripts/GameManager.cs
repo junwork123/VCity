5795d3f78be244a828cbf71a8659763030dd2bd9
LEEJESEOK <29701999+LEEJESEOK@users.noreply.github.com>
2021-09-24 16:20:07 +0900

    취소버튼으로 앱 종료, 가로화면으로 고정

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+#if UNITY_ANDROID
+        // 백버튼을 눌렀을 경우 게임 종료
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+#endif
+    }
+}
