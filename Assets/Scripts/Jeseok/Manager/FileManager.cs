8d8489cd48f3dc4e36d52d0435890fa96332d913
LEEJESEOK <junelle_js@naver.com>
2021-10-30 23:03:08 +0900

    Interaction 버튼 위치 조정, FileIO 테스트 씬 추가

---
+++ Assets/Scripts/Jeseok/Manager/FileManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class FileManager : Singleton<FileManager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
