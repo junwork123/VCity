735d0c3d1f77c3b25de98437fbe080027eb4179d
Junhyeok Choi <sposent7@naver.com>
2021-11-02 13:32:53 +0900

    메뉴스택 구현 / 이제 회원정보 입력창만 남았다

--- Assets/Scripts/Jun/MenuManager.cs
+++ Assets/Scripts/Jun/MenuManager.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    public static MenuManager Instance;//다른 class에서도 호출가능
-
-    [SerializeField] Menu[] menus;//SerializedField를 사용하면 우리는 public처럼 쓸 수 있지만  public이 아니여서 외부에서는 못만짐.
-
+    public static MenuManager Instance;
+    Stack<Menu> menuStack;
+    [SerializeField] Menu[] menus;
     private void Awake()
     {
         Instance = this;
+        menuStack = new Stack<Menu>();
     }
 
     public void OpenMenu(string menuName)
@@ -20,6 +20,8 @@ public class MenuManager : MonoBehaviour
             if (menus[i].menuName == menuName)//string을 받아서 해당이름 가진 메뉴를 여는 스크립트
             {
                 OpenMenu(menus[i]);
+                menuStack.Push(menus[i]);
+                return;
             }
             else if (menus[i].open)
             {
@@ -30,18 +32,35 @@ public class MenuManager : MonoBehaviour
 
     public void OpenMenu(Menu menu)
     {
-        for (int i = 0; i < menus.Length; i++)
-        {
-            if (menus[i].open)
-            {
-                CloseMenu(menus[i]);
-            }
-        }
         menu.Open();
+        menuStack.Push(menu);
+        return;
     }
 
     public void CloseMenu(Menu menu)
     {
-        menu.Close();
+        if (menuStack.Peek() == menu)
+        {
+            menu.Close();
+            menuStack.Pop();
+        }
+        else
+        {
+            Debug.Log("[Menu] : " + "가장 위에 열린 메뉴가 아닙니다");
+        }
+
+    }
+    private void Update()
+    {
+        //안드로이드인 경우
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape)) //뒤로가기 키 입력
+            {
+                CloseMenu(menuStack.Peek());
+                //처리할 내용
+            }
+
+        }
     }
 }
