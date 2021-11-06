2d41d636b46387aa880dac49c579bd2c0b1d9663
MaliciousJ <sposent7@naver.com>
2021-11-06 16:34:28 +0900

    merge

---
+++ Assets/Scripts/Jun/ToggleSetter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleSetter : MonoBehaviour
+{
+    [SerializeField]
+    Toggle[] toggles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
