e9c6dc35a36efcc7f7b4a0cda66f14870ac6cfc6
LEEJESEOK <29701999+LEEJESEOK@users.noreply.github.com>
2021-11-24 14:14:24 +0900

    Lightmap(City) 추가

--- Assets/Scripts/Jeseok/ButtonEvent/InteractionButtonEvent.cs
+++ Assets/Scripts/Jeseok/ButtonEvent/InteractionButtonEvent.cs
@@ -36,6 +36,7 @@ public class InteractionButtonEvent : MonoBehaviour
                 break;
 
             case InteractionType.MYPAGE:
+                
                 break;
 
             case InteractionType.EXIT:
